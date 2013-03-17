b2c28c4cdf1f996ea5fba796b52312253fa28e78
Long Nguyen Huu <hs@fuwar.in>
2013-03-17 21:08:00 +0100

    background stars
    a changé la taille de la fenêtre de jeu (à présent 1280*768 soit 40
    cases * 24 cases)
    a dessiné le 1er background de l'opening sequence : stars.png

--- source/constants.py
+++ source/constants.py
@@ -1,5 +1,5 @@
 # -*-coding:Latin-1 -*
 
-NB_SQUARES_PER_ROW = 20 # 20 cases sur une ligne
-NB_SQUARES_PER_COL = 10 # 10 cases sur une colonne
-SQUARE_SIDE = 32 # chaque case faire 32 px de côté
+NB_SQUARES_PER_ROW = 40 # nombre de cases sur une ligne
+NB_SQUARES_PER_COL = 24 # nombre de cases sur une colonne
+SQUARE_SIDE = 32 # chaque case fait 32 px de côté
